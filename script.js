0fb0d64d55180020925ee306085a5855a2c905c9
Guilherme Afonso <gui.p.afonso@gmail.com>
2025-01-20 22:47:54 -0300

    :tada:

---
+++ script.js
@@ -0,0 +1,16 @@
+function togglemode() {
+  const body = document.body
+
+
+  if(body.classList.contains ('light') ) {
+    body.classList.remove('light')
+  } else {
+    body.classList.add('light')
+  }
+
+
+
+
+
+
+}
